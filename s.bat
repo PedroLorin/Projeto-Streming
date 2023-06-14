b7273f8ecf273af475449abae47c836531312a6a
Matlogo <135663313+Matlogo@users.noreply.github.com>
2023-06-13 23:21:51 -0300

    Update s.bat

--- s.bat
+++ s.bat
@@ -576,7 +576,6 @@ void BuscaGenero(Serie *serie, int QuantidadeSeries)
     }//if
 }//void buscaGeneros
             
-            
 
 
 void salvaDados(Serie *serie, int QuantidadeSeries)
@@ -632,6 +631,7 @@ void salvaDados(Serie *serie, int QuantidadeSeries)
 int main() {
     int indiceHistorico = 0;
     int QuantidadeSeries = 258;
+    int menu;
     Serie *serie = (Serie*) malloc(QuantidadeSeries * sizeof(Serie));
 
 
@@ -641,7 +641,7 @@ int main() {
 
     if (arquivo == NULL) {
         perror("Erro ao abrir o arquivo Binario , tentaremos excutar o arquivo csv.\n");
-        
+
         FILE *arquivo = fopen("streaming_db.txt", "r");
 
          if (arquivo == NULL) {
@@ -650,30 +650,70 @@ int main() {
         }
         lerSeries(serie, QuantidadeSeries, arquivo);
         salvaDados(serie,QuantidadeSeries);
-        
+
     }else{
 
         lerSeriesBinario(serie, QuantidadeSeries, arquivo);
 
     }
 
-    imprimirSeries(serie, QuantidadeSeries);
-    //QuantidadeSeries = cadastrar(QuantidadeSeries, serie);
-    //apagar(QuantidadeSeries, serie);
-    //BuscaGenero(serie,QuantidadeSeries);
-   // Alterar(QuantidadeSeries, serie);
-   // Pesquisa(serie,QuantidadeSeries);
-   // indiceHistorico=salvaHistorico(serie,QuantidadeSeries,historico,indiceHistorico);
-    //salvaDados(serie,QuantidadeSeries);
 
 
+    printf("\n\nBusca Streaming\n\n");
+    printf("Digite 1 para mostrar todas as series\n");
+    printf("Digite 2 para buscar uma serie\n");
+    printf("Digite 3 para buscar um genero\n");
+    printf("Digite 4 para adicionar uma serie ao historico\n");
+    printf("Digite 5 para cadastrar uma serie nova\n");
+    printf("Digite 6 para alterar uma serie\n");
+    printf("Digite 7 para apagar uma serie\n");
+    printf("Digite 8 para salvar os seus dados\n");
+    printf("Digite 9 para sair\n");
+
+      scanf("%d",&menu);
+       switch (menu){
+
+         case 1:
+          imprimirSeries(serie, QuantidadeSeries);
+           break;
+
+         case 2: 
+          Pesquisa(serie,QuantidadeSeries);
+            break;
+
+         case 3:
+          BuscaGenero(serie,QuantidadeSeries);
+           break;
+
+         case 4:
+          indiceHistorico=salvaHistorico(serie,QuantidadeSeries,historico,indiceHistorico);
+           break;
+
+         case 5:
+          QuantidadeSeries = cadastrar(QuantidadeSeries, serie);
+            break;
+         case 6:
+          Alterar(QuantidadeSeries, serie);
+           break;
+
+         case 7: 
+          apagar(QuantidadeSeries, serie);
+           break;
+
+         case 8:
+          salvaDados(serie,QuantidadeSeries);
+           break;
+
+         case 9:
+           break; 
+       }
 
 
 
     free(serie);
     free (historico);
     fclose(arquivo);
-    
+
 
     return 0;
 }
